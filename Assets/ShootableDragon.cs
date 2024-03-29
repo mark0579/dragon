4cb65431a8397af2e7f006e30d199a62dfdcc85a
seen2213 <ktn6005@gmail.com>
2024-03-29 16:20:33 +0900

    gun

---
+++ Assets/ShootableDragon.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShootableDragon : MonoBehaviour
+{
+
+	//The box's current health point total
+	public float currentHealth = 3;
+
+	public void Damage(float damageAmount)
+	{
+		//subtract damage amount when Damage function is called
+		currentHealth -= damageAmount;
+
+		//Check if health has fallen below zero
+		if (currentHealth <= 0)
+		{
+			//if health has fallen below zero, deactivate it 
+			gameObject.SetActive(false);
+		}
+	}
+	public float GetCurrentHealth()
+	{
+		return currentHealth;
+	}
+}
+
